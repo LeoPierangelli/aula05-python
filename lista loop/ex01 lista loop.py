872a352ff707c4b00d84a154f5322ae2c2bc191a
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-09-25 10:37:12 -0300

    ex01 gabaritado

--- lista loop/ex01 lista loop.py
+++ lista loop/ex01 lista loop.py
@@ -1,6 +1,15 @@
-min = 0
+'''min = 0
 max = 10
 nota = int(input(f'Informe uma nota de {min} a {max}: '))
 while not (nota >= min and nota <= max):
     nota = int(input('Digite um valor válido: '))
-print(nota)
+print(nota)'''
+
+while True:
+    num = input('Digite um número: ')
+    if num.isnumeric():
+        num = int(num)
+        if num > 0 and num < 10:
+            print('Boa')
+            break
+
